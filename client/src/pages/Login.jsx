24f211289910146cb956d6e73f3798f2187507c9
Felix Owolabi <owolabifelix78@gmail.com>
2023-08-09 14:05:48 +0100

    added a login Page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,25 @@
+import { Alert,Button,Form,Row,Col,Stack} from 'react-bootstrap';
+
 const Login = () => {
-    return (
-         <>
-         Login
-        </> 
-    );
+    return ( 
+          <>
+          <Form>
+            <Row style={{height:'80vh',justifyContent:'center',paddingTop:'7%'}}>
+                <Col xs={6}>
+                    <Stack gap={3}>
+                      <h2>LogIn</h2>
+                      <Form.Control type='email' placeholder='Email'/>
+                      <Form.Control type='password' placeholder='Password'/>
+                      <Button variant='primary' type='submit'>LogIn</Button>
+                      <Alert variant='danger'>
+                        <p>An error occurred!</p>
+                      </Alert>
+                    </Stack>
+                </Col>
+            </Row>
+          </Form>
+          </>
+     );
 }
  
 export default Login;
