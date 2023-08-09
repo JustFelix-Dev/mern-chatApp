aaf5b2164a40d1f99d1f91c6f66fb170a2f8e38e
Felix Owolabi <owolabifelix78@gmail.com>
2023-08-09 13:04:29 +0100

    added react routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,25 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import {Routes,Route,Navigate} from 'react-router-dom'
+import Chat from './pages/Chat'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container} from 'react-bootstrap';
+
 
 function App() {
 
   return (
-    <>
-    
-    </>
+    <Container className='bg-danger'>
+     <Routes>
+      <Route path='/' element={<Chat/>}/>
+      <Route path='/register' element={<Register/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='*' element={<Navigate to={'/'}/>}/>
+     </Routes>
+    </Container>
   )
 }
 
