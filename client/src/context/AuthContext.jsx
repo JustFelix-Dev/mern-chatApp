8300430a542f6154ce1ab2ad34defb118b58207a
Felix Owolabi <owolabifelix78@gmail.com>
2023-08-10 10:53:28 +0100

    authContext:provided successfully

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -7,8 +7,14 @@ import { baseUrl, postRequest } from '../utils/services';
     const [ user,setUser ] = useState(null);
     const [ registerError,setRegisterError ] = useState(null);
     const [ isRegisterLoading, setIsRegisterLoading] = useState(false);
+    const [ loginError,setLoginError ] = useState(null);
+    const [ isLoginLoading, setIsLoginLoading] = useState(false);
     const [ registerInfo,setRegisterInfo] = useState({
         name: '',
+        email: '',
+        password: ''
+    });
+    const [ loginInfo,setLoginInfo] = useState({
         email: '',
         password: ''
     });
@@ -19,11 +25,15 @@ import { baseUrl, postRequest } from '../utils/services';
         console.log(user)
     },[])
 
-    console.log("Register Info:", registerInfo);
+    console.log("LoginInfo:", loginInfo);
 
      const updateRegisterInfo = useCallback((info)=>{
           setRegisterInfo(info);
      },[]);
+
+     const updateLoginInfo = useCallback((info)=>{
+        setLoginInfo(info);
+   },[]);
 
      const registerUser = useCallback(async(e)=>{
         e.preventDefault();
@@ -39,6 +49,21 @@ import { baseUrl, postRequest } from '../utils/services';
       setUser(response) 
      },[registerInfo])
 
+     const loginUser = useCallback(async(e)=>{
+          e.preventDefault();
+          setIsLoginLoading(true)
+          setLoginError(null)
+          const response = await postRequest(`${baseUrl}/users/login`,JSON.stringify(loginInfo))
+          setIsLoginLoading(false)
+          if(response.error){
+               return setLoginError(response)
+          }
+
+          localStorage.setItem('User', JSON.stringify(response))
+        setUser(response) 
+
+     },[loginInfo])
+
      const logoutUser = useCallback(()=>{
            localStorage.removeItem('User');
            setUser(null);
@@ -47,8 +72,9 @@ import { baseUrl, postRequest } from '../utils/services';
     return(
             <AuthContext.Provider value={{user,registerInfo,
                                         updateRegisterInfo,registerUser,
-                                         registerError,isRegisterLoading,logoutUser}}>
-            {children}
+                                         registerError,isRegisterLoading,
+                                         logoutUser,loginUser,loginInfo,updateLoginInfo,isLoginLoading,loginError}}>
+                 {children}
             </AuthContext.Provider>
     )
  }
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,19 +1,22 @@
 import { Alert,Button,Form,Row,Col,Stack} from 'react-bootstrap';
+import { AuthContext } from '../context/AuthContext';
+import { useContext } from 'react';
 
 const Login = () => {
+    const { loginUser,loginInfo,updateLoginInfo,isLoginLoading,loginError } = useContext(AuthContext);
     return ( 
           <>
-          <Form>
+          <Form onSubmit={loginUser}>
             <Row style={{height:'80vh',justifyContent:'center',paddingTop:'7%'}}>
                 <Col xs={6}>
                     <Stack gap={3}>
                       <h2>LogIn</h2>
-                      <Form.Control type='email' placeholder='Email'/>
-                      <Form.Control type='password' placeholder='Password'/>
-                      <Button variant='primary' type='submit'>LogIn</Button>
-                      <Alert variant='danger'>
-                        <p>An error occurred!</p>
-                      </Alert>
+                      <Form.Control type='email' placeholder='Email' onChange={(e)=> updateLoginInfo({...loginInfo,email:e.target.value})} />
+                      <Form.Control type='password' placeholder='Password' onChange={(e)=> updateLoginInfo({...loginInfo,password:e.target.value})}/>
+                      <Button variant='primary' type='submit'>{ isLoginLoading ? 'Submitting...':"LogIn"}</Button>
+                      { loginError?.error && <Alert variant='danger'>
+                        <p>{loginError?.message}</p>
+                      </Alert>}
                     </Stack>
                 </Col>
             </Row>
